0feb4de8b89b2cd47d22a833e9f4e19e20795f4f
nesto2001 <tanlong6121@gmail.com>
2024-03-27 04:08:15 +0700

    fix responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,12 +21,12 @@ const index = () => {
   return (
     <div className="w-full flex justify-center items-center flex-col">
       <div className="header__logo mb-5 -mt-20 flex items-center">
-        <span className="w-[130px] overflow-hidden ">
-          <Image width={150} src={Logo} preview={false} />
+        <span className="w-[88px] lg:w-[130px] overflow-hidden ">
+          <img className="lg:w-[150px] w-[80px]" src={Logo} />
         </span>
         <h1 className="font-bold text-3xl text-body">Phuc An Infotel</h1>
       </div>
-      <Card className="w-1/4">
+      <Card className="w-4/5 lg:w-1/4">
         <h3 className="text-2xl font-bold mb-5 text-center text-body">
           Sign in
         </h3>
